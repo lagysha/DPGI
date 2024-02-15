e18de9e5bc3f44ba4c9ad4571f2ab6cc829df682
Nikita Malov <nikita.volam@gmail.com>
2024-02-15 11:55:12 +0200

    add docs program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+/// <summary>
+/// Головний клас програми.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Точка входу в програму.
+    /// </summary>
+    /// <param name="args">Масив аргументів командного рядка.</param>
+    static void Main(string[] args)
+    {
+        /// <summary>
+        /// Виведення на консоль рядка "Hello, World!"
+        /// </summary>
+        Console.WriteLine("Hello, World!");
+    }
+}
